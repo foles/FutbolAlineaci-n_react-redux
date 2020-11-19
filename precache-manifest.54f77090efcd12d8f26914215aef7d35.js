07ba8e0c4b24b23ee06ee58b8914154d2f0a679e
foles <foles4h@gmail.com>
2020-11-18 20:43:58 -0400

    Updates

---
+++ precache-manifest.54f77090efcd12d8f26914215aef7d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92601d1ceb5953ba77b98f990d6e03c9",
+    "url": "/FutbolAlineaci-n_react-redux/index.html"
+  },
+  {
+    "revision": "5a00a962cce423a39e82",
+    "url": "/FutbolAlineaci-n_react-redux/static/css/main.b67280ea.chunk.css"
+  },
+  {
+    "revision": "116b8d5f1ed2e2abe371",
+    "url": "/FutbolAlineaci-n_react-redux/static/js/2.6d61ed6c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FutbolAlineaci-n_react-redux/static/js/2.6d61ed6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a00a962cce423a39e82",
+    "url": "/FutbolAlineaci-n_react-redux/static/js/main.3d4848b1.chunk.js"
+  },
+  {
+    "revision": "b74dc7cf3dff8aee64c4",
+    "url": "/FutbolAlineaci-n_react-redux/static/js/runtime-main.bb3adae2.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/FutbolAlineaci-n_react-redux/static/media/cancha.aa6d54bf.svg"
+  }
+]);
